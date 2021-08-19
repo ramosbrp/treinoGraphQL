750ffa4966bbbb54669bda437c1c31f93acf777e
Bruno <bruno.rp45@gmail.com>
2021-08-19 14:53:02 -0300

    Parâmetros e redirecionamento

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import { readFile } from 'fs';
 import { resolve } from 'path';
+import { parse } from 'querystring';
 
 const server = createServer((req, res) => {
     switch (req.url) {
@@ -21,24 +22,46 @@ const server = createServer((req, res) => {
                     res.writeHead(500, "Can't process HTML file.");
                     res.end();
                     return;
-                }
+                };
+                res.writeHead(200);
+                res.write(file);        
+                res.end();
+            });
+            break;
+        };
+        case '/home': {
+            const path = resolve(__dirname, './pages/home.html');
+            readFile(path, (error, file) => {
+                if (error) {
+                    res.writeHead(500, "Can't process HTML file.");
+                    res.end();
+                    return;
+                };
                 res.writeHead(200);
                 res.write(file);
                 res.end();
-            })
+            });
             break;
         }
         case '/authenticate': {
-            
+            let data = '';
+            req.on('data', (chunk) => {
+                data += chunk;
+            });
+            req.on('end', () => {
+                const params = parse(data);
+                res.writeHead(301, {
+                    Location: '/home'
+                });
+                res.end();
+            });
             break;
-        }
+        };
         default: {
             res.writeHead(404, 'Service not found.');
-            res.write('Okay');
             res.end();
-        }
-           
-    }
+        };           
+    };
 });
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
