5764a66d6b6e973cd2c798b7551eab58d73d3341
Bruno <bruno.rp45@gmail.com>
2021-08-22 20:58:36 -0300

    Pacote com React.js - Rotas com react-router-dom  -  Não está funcionando, tá dando erro

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile } from 'fs';
-import { resolve } from 'path';
 import { parse } from 'querystring';
 
 const server = createServer((req, res) => {
@@ -15,44 +13,14 @@ const server = createServer((req, res) => {
             res.end();
             break;
         }
-        case '/sign-in': {
-            const path = resolve(__dirname, './pages/sign-in.html');
-            readFile(path, (error, file) => {
-                if (error) {
-                    res.writeHead(500, "Can't process HTML file.");
-                    res.end();
-                    return;
-                };
-                res.writeHead(200);
-                res.write(file);        
-                res.end();
-            });
-            break;
-        };
-        case '/home': {
-            const path = resolve(__dirname, './pages/home.html');
-            readFile(path, (error, file) => {
-                if (error) {
-                    res.writeHead(500, "Can't process HTML file.");
-                    res.end();
-                    return;
-                };
-                res.writeHead(200);
-                res.write(file);
-                res.end();
-            });
-            break;
-        }
+
         case '/authenticate': {
             let data = '';
             req.on('data', (chunk) => {
                 data += chunk;
             });
             req.on('end', () => {
-                const params = parse(data);
-                res.writeHead(301, {
-                    Location: '/home'
-                });
+                const params = parse(data);                
                 res.end();
             });
             break;
